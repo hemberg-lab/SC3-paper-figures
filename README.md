3ccafaadc7e2ccc475049ce040a928c7ace8ea26
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2017-02-24 14:45:29 +0000

    add S1 and S2 figs

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SC3-paper-figures
 Scripts to recreate figures from the SC3 paper
+
+## Session Info
+
+```
+R Under development (unstable) (2016-10-20 r71540)
+Platform: x86_64-apple-darwin13.4.0 (64-bit)
+Running under: OS X Yosemite 10.10.5
+
+locale:
+[1] en_GB.UTF-8/en_GB.UTF-8/en_GB.UTF-8/C/en_GB.UTF-8/en_GB.UTF-8
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+[1] dplyr_0.5.0   ggplot2_2.2.1
+
+loaded via a namespace (and not attached):
+ [1] Rcpp_0.12.8      digest_0.6.10    assertthat_0.1   grid_3.4.0       R6_2.2.0         plyr_1.8.4      
+ [7] gtable_0.2.0     DBI_0.5-1        magrittr_1.5     scales_0.4.1     stringi_1.1.2    reshape2_1.4.2  
+[13] lazyeval_0.2.0   labeling_0.3     tools_3.4.0      stringr_1.1.0    munsell_0.4.3    colorspace_1.3-1
+[19] tibble_1.2     
+```
---
+++ S1.R
@@ -0,0 +1,27 @@
+library(ggplot2)
+library(dplyr)
+
+d <- read.csv("S1.csv")
+d <- d[d$Dataset %in% c("Biase", "Yan", "Goolam"), ]
+
+d_med <- d %>%
+    group_by(Dataset, gene_filter, Distance, Transformation, d) %>%
+    dplyr::summarise(ARI = median(ARI))
+
+d$Group <- paste(d$Transformation, d$d)
+d$Dataset <- factor(d$Dataset, levels = c("Biase", "Yan", "Goolam"))
+d_med$Dataset <- factor(d_med$Dataset, levels = c("Biase", "Yan", "Goolam"))
+
+p1 <- ggplot(d, aes(d, ARI, group = Group, color = Transformation)) +
+    geom_boxplot(outlier.size = 0.3, width = 1) +
+    facet_grid(Dataset + Distance ~ gene_filter, scales = "free_x") +
+    labs(x = "d as % of N", y = "ARI") +
+    geom_vline(xintercept = c(4, 7)) +
+    geom_line(data = d_med, aes(group = Transformation), size = 0.5) +
+    theme_classic(base_size = 8) +
+    theme(axis.line=element_blank(),
+          strip.background = element_rect(colour = "white")) +
+    annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf, color = "black")+
+    annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf, color = "black")
+
+ggsave("S1.jpeg", w = 9, h = 6)
